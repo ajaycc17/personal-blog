ad360760311a6c3ef4c50c3373df3c9ec48e8707
Ajay Choudhury <ajaycc17@gmail.com>
2023-08-06 22:16:07 +0530

    fix: expect server error nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
     return (
         <div className="fade-in">
             <Hero image="Ajay.jpeg" />
-            {/* @ts-expect-error Server Component */}
             <Articles />
             <Collections />
         </div>
